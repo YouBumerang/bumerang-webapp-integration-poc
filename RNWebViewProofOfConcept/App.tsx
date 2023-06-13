c2aa76ae7371a41d4725cb82cedee441802207e8
Adam El Amrani Colina <adelamco1091@outlook.com>
2023-06-13 09:40:18 +0200

    Add swift and react native proof of concept

---
+++ RNWebViewProofOfConcept/App.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import {
+  Modal,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+  useColorScheme,
+  StyleSheet,
+} from 'react-native';
+import {WebView} from 'react-native-webview';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [showWebView, setShowWebView] = React.useState(false);
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <TouchableOpacity
+          onPress={() => setShowWebView(true)}
+          style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Button</Text>
+          </View>
+        </TouchableOpacity>
+        <Modal
+          visible={showWebView}
+          animationType="slide"
+          transparent={true}
+          onRequestClose={() => setShowWebView(false)}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalHeader}>
+              <TouchableOpacity onPress={() => setShowWebView(false)}>
+                <Text style={styles.closeButtonText}>Close</Text>
+              </TouchableOpacity>
+            </View>
+            <WebView
+              onMessage={event => {
+                console.log(event.nativeEvent.data);
+                setShowWebView(false);
+              }}
+              source={{
+                uri: 'https://bumerang-webview.ew.r.appspot.com/',
+              }}
+              style={styles.webView}
+            />
+          </View>
+        </Modal>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'black',
+    borderRadius: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    top: 50,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    padding: 10,
+    backgroundColor: '#f5f5f5',
+  },
+  webView: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  closeButtonText: {
+    color: 'blue',
+  },
+});
+
+export default App;
