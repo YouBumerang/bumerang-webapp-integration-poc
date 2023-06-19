abd2f678d561e9245f4870e12d1779895865949b
Adam El Amrani Colina <adelamco1091@outlook.com>
2023-06-19 08:02:31 +0200

    Add android project

---
+++ android/Androidwebview/app/src/main/java/com/example/android_webview/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.android_webview;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.webkit.JavascriptInterface;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private Button openButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        openButton = findViewById(R.id.openButton);
+
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebView();
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+
+    }
+
+    private void openWebView() {
+        webView.setVisibility(View.VISIBLE);
+        openButton.setVisibility(View.GONE);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new JavaScriptInterface(), "parent");
+        webView.loadUrl("http://192.168.1.134:3000/test");
+    }
+
+    private class JavaScriptInterface {
+        @JavascriptInterface
+        public void receiveMessage(String message) {
+            // Handle the received post message here
+            Log.i("mensaje", "test");
+            Toast.makeText(MainActivity.this, "Received Post Message: " + message, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
